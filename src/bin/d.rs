6be9c3849ae3106f5bd442c43b57cb33c4061a2a
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-26 23:09:05 +0900

    d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,3 +1,13 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        k: usize,
+        ps: [i32; n],
+    }
+
+    for num in k..=n {
+        let mut array = ps.iter().take(num).collect::<Vec<&i32>>();
+        array.sort_by_key(|&x| std::cmp::Reverse(x));
+        println!("{}", array[k - 1]);
+    }
 }
