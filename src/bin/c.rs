5a6872aa952af19ceabe4467dfb20ab2367fd4f9
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-26 22:25:34 +0900

    c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,3 +1,14 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        k: i64,
+    }
+
+    let ans = (0..63).into_iter()
+                     .map(|x| k & (1 << x) != 0)
+                     .map(|x| if x {'2'} else {'0'})
+                     .rev()
+                     .skip_while(|&x| x == '0')
+                     .collect::<String>();
+
+    println!("{}", ans)
 }
