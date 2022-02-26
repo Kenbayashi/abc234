1c2f4b9d848743bdb00e3f641fff735ff32c37ed
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-26 21:42:50 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,13 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        t: i32,
+    }
+
+    let ans = func(func(func(t) + t) + func(func(t)));
+
+    println!("{}", ans);
+}
+
+fn func(t: i32) -> i32 {
+    t * t + 2 * t + 3
 }
